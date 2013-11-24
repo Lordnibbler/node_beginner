2765b8207346fc321a60a6d7c38fba4bead8cc1f
Ben Radler <ben@benradler.com>
2013-11-24 15:19:56 -0800

    Add show requestHandler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
 var querystring = require('querystring');
+var fs          = require('fs');
 
 function start(response, postData) {
   console.log('Request handler "start" was called');
@@ -10,7 +11,9 @@ function start(response, postData) {
     '</head>'+
     '<body>'+
       '<form action="/upload" method="post">'+
-      '<textarea name="text" rows="20" cols="60"></textarea>'+ '<input type="submit" value="Submit text" />'+ '</form>'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+      '</form>'+
     '</body>'+
   '</html>';
 
@@ -26,5 +29,21 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData) {
+  console.log('Request handler "show" was called');
+  fs.readFile("tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
 exports.start  = start;
 exports.upload = upload;
+exports.show   = show;
